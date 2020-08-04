4eac91cff92c5ede27a81d32413d0ec5aed81862
Debmalya Pramanik <debmalyapramanik.005@gmail.com>
2020-08-04 15:30:09 +0530

    Added new Geo-Functions
    * two new functions are calculate_bearing_angle() and calculate_point_at_bearing

--- agine/config.py
+++ agine/config.py
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
 
 '''Config File - for CONSTANTS'''
+R = 6371000 # Radius of the Earth
+
 CRS     = {'init' : 'epsg:4326'}
 DATUM   = 'WGS84'
 UNITS   = ['m', 'km', 'rad', 'none']
